6747eae9b7fdfdd5dbecabb3e084bf00f1e164b1
selenium <aleksandrstomsans@outlook.com>
2026-08-11 16:24:12 +0300

    Playwright

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -21,6 +21,7 @@ private static Page page;
 
 @BeforeEach
 void setUp() {
+     
      playwright = Playwright.create();
      browser = playwright.chromium().launch(
                new BrowserType.LaunchOptions().setHeadless(false));
